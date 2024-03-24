35b1aa05cb5a9ed260317145e7be756eab5ec7ea
joseluis <joseluis.universidad2020@gmail.com>
2024-03-23 21:48:02 -0400

    Nuevo comienzo

--- src/controllers/consultas.controller.js
+++ src/controllers/consultas.controller.js
@@ -2,6 +2,7 @@ import OpenAI from "openai"
 import 'dotenv/config'
 
 const apiKey = process.env.OPENAI_API_KEY
+const prompt = process.env.MI_PROMPT
 const openai = new OpenAI({ apiKey})
 
 async function traduciendo(consulta) {
@@ -9,7 +10,6 @@ async function traduciendo(consulta) {
         const respuesta = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
             messages: [
-                // { role: 'system', content: 'Dame la palabra y el idioma al que quieres traducir'},
                 { role: 'user', content: consulta}
             ]
         })
@@ -21,17 +21,12 @@ async function traduciendo(consulta) {
 }
 
 export const traducir = async (req, res) => {
-    console.log("MENSAJE RECIBIDO: "+req.body.mensaje)
-    console.log(req.body)
-    const palabraIdioma = req.body.mensaje
-    const texto = "A continuacion voy a enviarte unos parametros. La primera palabra es la que quiero traducir, y la segunda palabra el idioma al que quiero traducir. Tu respuesa sera SOLAMENTE LA PALABRA TRADUCIDA. Por ejemplo si te mando 'hola,ingles', tu respuesta sera 'hello'. Estos son los parametros "+palabraIdioma
+    const texto = prompt + req.body.mensaje
     const mensaje = await traduciendo(texto)
-    console.log(mensaje)
-    // console.log(req.body.mensaje)
     res.json(mensaje)
 }
 
 export const saludo = (req, res) => {
     console.log("Saludos")
-    res.json("Saludos Dos")
+    res.json("Saludos RES")
 }
